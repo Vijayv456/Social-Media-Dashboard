248496a59bb9fe281088e8c570c778e98271efa6
Vijayv456 <127855231+Vijayv456@users.noreply.github.com>
2023-03-15 19:15:23 +0530

    Add files via upload

---
+++ dark mode.js
@@ -0,0 +1,83 @@
+function myFunction(){
+   
+    var instagram=document.querySelector(".instagram1");
+    var facbook1=document.querySelector(".facbook1");
+    var twitter=document.querySelector(".twitter");
+    var youtube=document.querySelector(".youtube");
+    var darkmode=document.getElementById("dark-mode");
+    var box1=document.getElementById("1");
+    var box2=document.getElementById("2");
+    var box3=document.getElementById("3");
+    var box4=document.getElementById("4");
+    var box5=document.getElementById("5");
+    var box6=document.getElementById("6");
+    var box7=document.getElementById("7");
+    var box8=document.getElementById("8");
+    var boxn1=document.getElementById("n1")
+    var boxn2=document.getElementById("n2")
+    var boxn3=document.getElementById("n3")
+    var boxn4=document.getElementById("n4")
+    var boxn5=document.getElementById("n5")
+    var boxn6=document.getElementById("n6")
+    var boxn7=document.getElementById("n7")
+    var boxn8=document.getElementById("n8")
+
+if(darkmode.checked==true){
+    document.getElementById("body").style.backgroundColor="black";
+document.getElementById("body").style.color="white"
+instagram.style.backgroundColor=" hsl(228, 28%, 20%)"
+instagram.style.color="white";
+facbook1.style.backgroundColor=" hsl(228, 28%, 20%)";
+facbook1.style.color="white";
+twitter.style.backgroundColor=" hsl(228, 28%, 20%)";
+twitter.style.color="white";
+youtube.style.backgroundColor=" hsl(228, 28%, 20%)";
+youtube.style.color="white";
+box1.style.backgroundColor=" hsl(228, 28%, 20%)";
+box2.style.backgroundColor=" hsl(228, 28%, 20%)";
+box3.style.backgroundColor=" hsl(228, 28%, 20%)";
+box4.style.backgroundColor=" hsl(228, 28%, 20%)";
+box5.style.backgroundColor=" hsl(228, 28%, 20%)";
+box6.style.backgroundColor=" hsl(228, 28%, 20%)";
+box7.style.backgroundColor=" hsl(228, 28%, 20%)";
+box8.style.backgroundColor=" hsl(228, 28%, 20%)";
+boxn1.style.color="white";
+boxn2.style.color="white";
+boxn3.style.color="white";
+boxn4.style.color="white";
+boxn5.style.color="white";
+boxn6.style.color="white";
+boxn7.style.color="white";
+boxn8.style.color="white";
+}
+    else{
+        document.getElementById("body").style.backgroundColor="white"
+        document.getElementById("body").style.color="black"
+        instagram.style.backgroundColor="hsl(225, 100%, 98%)"
+instagram.style.color="black";
+facbook1.style.backgroundColor="hsl(225, 100%, 98%)"
+facbook1.style.color="black";
+twitter.style.backgroundColor="hsl(225, 100%, 98%)"
+twitter.style.color="black";
+youtube.style.backgroundColor="hsl(225, 100%, 98%)"
+youtube.style.color="black";
+box1.style.backgroundColor="hsl(225, 100%, 98%)";
+box2.style.backgroundColor="hsl(225, 100%, 98%)"
+box3.style.backgroundColor="hsl(225, 100%, 98%)"
+box4.style.backgroundColor="hsl(225, 100%, 98%)"
+box5.style.backgroundColor="hsl(225, 100%, 98%)"
+box6.style.backgroundColor="hsl(225, 100%, 98%)"
+box7.style.backgroundColor="hsl(225, 100%, 98%)"
+box8.style.backgroundColor="hsl(225, 100%, 98%)"
+boxn1.style.color="black";
+boxn2.style.color="black";
+boxn3.style.color="black";
+boxn4.style.color="black";
+boxn5.style.color="black";
+boxn6.style.color="black";
+boxn7.style.color="black";
+boxn8.style.color="black";
+
+
+    }
+}
